b2be2d3c492f05c1ed121da2efd817370590193c
LaislaSte <stephenielaisla@gmail.com>
2022-10-05 17:32:14 -0300

    app heroku

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Crud Laravel</title>
+    </head>
+    <body>
+        <center>
+            <h1>CRUD</h1>
+
+            <a href="/create_pessoa"><button>Cadastrar Usuário</button></a>
+            &nbsp;&nbsp;&nbsp;
+            <a href="/listar_pessoas"><button>Listar Usuários</button></a>
+        </center>
+    </body>
+</html>
